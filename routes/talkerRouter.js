547254f28d49f862a029f964933fce8f0aeebc08
Samuel Constantino <samuelicapuidados@gmail.com>
2021-10-28 11:13:05 -0300

    desafio 2

--- routes/talkerRouter.js
+++ routes/talkerRouter.js
@@ -6,12 +6,28 @@ const rescue = require('express-rescue');
 
 const router = express.Router();
 
-router.get('/', rescue(async (_res, res, _next) => {
-    const talkersJson = await fs.readFile('./talker.json');
+const TALKER_FILE = './talker.json';
+
+router.get('/', rescue(async (_req, res, _next) => {
+    const talkersJson = await fs.readFile(TALKER_FILE);
 
     const talkers = await JSON.parse(talkersJson);
     
     return res.status(200).json(talkers);
+}));
+
+router.get('/:id', rescue(async (req, res, _next) => {
+    const { id } = req.params;
+
+    const talkersJson = await fs.readFile(TALKER_FILE);
+
+    const talkers = JSON.parse(talkersJson);
+
+    const talker = talkers.find((t) => t.id === +id);
+
+    if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+    return res.status(200).json(talker);
 }));
 
 module.exports = router;
